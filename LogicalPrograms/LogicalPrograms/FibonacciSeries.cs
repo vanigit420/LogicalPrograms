c33bc1e3d022d0eab208808014fd0469a2ad0918
shivani korwan <shivanikorwan63232@gmail.com>
2022-06-11 11:01:47 +0530

    print fibonacci series

---
+++ LogicalPrograms/LogicalPrograms/FibonacciSeries.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogicalPrograms
+{
+    public class FibonacciSeries
+    {
+        public static void PrintingFibonacci()
+        {
+            int n1 = 0, n2 = 1, n3, i, number;
+
+            Console.WriteLine("Enter the number of elements");
+            number = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write(n1 + " " + n2 + " ");
+
+            for (i = 2; i < number; i++)
+            {
+                n3 = n1 + n2;
+
+                Console.Write(n3 + " ");
+
+                n1 = n2;
+                n2 = n3;
+            }
+        }
+    }
+}
---
+++ LogicalPrograms/LogicalPrograms/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Threading;
+using System.Diagnostics;
+
+
+namespace LogicalPrograms
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //ReverseNumber.PrintTheReverseNumber();
+
+            FibonacciSeries.PrintingFibonacci();
+
+
+        }
+    }
+}
